d03f715f0eaffac7c61ce91db6e573bbce833f29
Pavel Kurzaev <espakurzaev@gmail.com>
2022-05-14 11:12:00 +0200

    format option added

--- gendiff.js
+++ gendiff.js
@@ -3,12 +3,16 @@
 import { Command } from 'commander';
 const program = new Command();
 
-program
-  .name('gendiff')
+program.name('gendiff')
   .description('Compares two configuration files and shows a difference')
   .version('0.1.0');
 
-program.parse(process.argv);
+program.option('-f, --format <type>', 'output format');
+
+program.argument('<filepath1>')
+  .argument('<filepath2>');
+
+program.parse();
 
 /*
 
