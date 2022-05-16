6973df7ab9840895b1443c25e17dbe5de355b870
Pavel Kurzaev <espakurzaev@gmail.com>
2022-05-16 17:10:13 +0200

    delete files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,31 +1,31 @@
 // @ts-check
 import { test, expect } from '@jest/globals';
-import { compareObjects } from '../src/calcdiff.js';
+import { genDiff, compareObjects } from '../src/calcdiff.js';
 
-const res1 = '{\n}';
-const res2 = '{\n  - name: vasya\n}';
-const res3 = '{\n  + name: vasya\n}';
+test('test objects empty', () => {
+  const res1 = '{\n}';
+  const res2 = '{\n  - name: vasya\n}';
+  const res3 = '{\n  + name: vasya\n}';
 
-test('test empty', () => {
-    expect(compareObjects([], [])).toEqual(res1);
-    expect(compareObjects([["name", "vasya"]], [])).toEqual(res2);
-    expect(compareObjects([], [["name", "vasya"]])).toEqual(res3);
+  expect(compareObjects([], [])).toEqual(res1);
+  expect(compareObjects([["name", "vasya"]], [])).toEqual(res2);
+  expect(compareObjects([], [["name", "vasya"]])).toEqual(res3);
 });
 
-test('same key', () => {
-    const obj1 = [
-        ["node", "hexlet"],
-        ["ip", "192.172.15.1"],
-        ["version", "1.0"]
-    ];
-    const obj2 = [
-        ["node", "hexlet"],
-        ["ip", "192.172.15.1"],
-        ["version", "1.1"]
-    ];
-    const res = '{\n    node: hexlet\n    ip: 192.172.15.1\n  - version: 1.0\n  + version: 1.1\n}';
+test('objects same key', () => {
+  const obj1 = [
+    ["node", "hexlet"],
+    ["ip", "192.172.15.1"],
+    ["version", "1.0"]
+  ];
+  const obj2 = [
+    ["node", "hexlet"],
+    ["ip", "192.172.15.1"],
+    ["version", "1.1"]
+  ];
+  const res = '{\n    node: hexlet\n    ip: 192.172.15.1\n  - version: 1.0\n  + version: 1.1\n}';
 
-    expect(compareObjects(obj1, obj2)).toEqual(res);
+  expect(compareObjects(obj1, obj2)).toEqual(res);
 });
 
 
