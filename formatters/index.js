28ea3edc37f4310b70f9c5f8531ba9d605b1cb4a
Pavel Kurzaev <espakurzaev@gmail.com>
2022-05-18 09:15:45 +0200

    add json

--- formatters/index.js
+++ formatters/index.js
@@ -1,8 +1,11 @@
 import { printStylish } from './stylish.js';
 import { printPlain } from './plain.js';
+import { printJSON } from './json.js';
 
 const printResult = (result, outputStyle) => {
   switch (outputStyle) {
+  case 'json':
+    return printJSON(result).flat(10).join('\n');
   case 'plain':
     return printPlain(result).flat(10).join('\n');
   case 'stylish':
