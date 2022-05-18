21c2f7cc62f49638cd12b14b1aeab49ec5a6041c
Pavel Kurzaev <espakurzaev@gmail.com>
2022-05-18 11:31:51 +0200

    test only with files

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,33 +1,6 @@
 // @ts-check
 import { test, expect } from '@jest/globals';
-import { genDiff, compareObjects, printResult } from '../src/gendiff.js';
-
-test('test objects empty', () => {
-  const res1 = '{\n}';
-  const res2 = '{\n  - name: vasya\n}';
-  const res3 = '{\n  + name: vasya\n}';
-
-  expect(printResult(compareObjects([], []))).toEqual(res1);
-  expect(printResult(compareObjects([["name", "vasya"]], []))).toEqual(res2);
-  expect(printResult(compareObjects([], [["name", "vasya"]]))).toEqual(res3);
-});
-
-test('objects same key unsorted', () => {
-  const obj1 = [
-    ["node", "hexlet1"],
-    ["ip", "192.172.15.1"],
-    ["version", "1.0"]
-  ];
-  const obj2 = [
-    ["node", "hexlet2"],
-    ["ip", "192.172.15.1"],
-    ["version", "1.1"]
-  ];
-  const res = '{\n    ip: 192.172.15.1\n  - node: hexlet1\n  + node: hexlet2\n  - version: 1.0\n  + version: 1.1\n}';
-
-  expect(printResult(compareObjects(obj1, obj2))).toEqual(res);
-});
-
+import genDiff from '../src/gendiff.js';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { readFileSync } from 'fs';
