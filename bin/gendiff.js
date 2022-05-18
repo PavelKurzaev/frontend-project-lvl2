f7679c11bb4e61ee00c263e9471e6af98ec2c53e
Pavel Kurzaev <espakurzaev@gmail.com>
2022-05-18 11:25:30 +0200

    refactor bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,20 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
-import { genDiff } from '../src/gendiff.js'
+import { Command, Option } from 'commander';
+import genDiff from '../src/gendiff.js';
 
 const program = new Command();
 
-program.name('gendiff')
-  .description('Compares two configuration files and shows a difference')
-  .version('0.1.0')
-  .argument('<filepath1>').argument('<filepath2>')
-  .option('-f, --format <type>', 'output format', 'stylish')
-  .action((filepath1, filepath2, options) => console.log(genDiff(filepath1, filepath2, options.format)));
+program
+  .name('gendiff')
+  .usage('[options] <filepath1> <filepath2>')
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .helpOption('-h, --help', 'output usage information')
+  .addOption(new Option('-f, --format [type]', 'output format').choices(['stylish', 'plain', 'json']).default('stylish'))
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format));
+  });
 
 program.parse();
